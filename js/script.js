55787568249f54159ee59149c30f650b350f900e
EmiPro97 <emi.proie97@gmail.com>
2021-04-11 22:29:13 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 $(document).ready(function(){
     // References
     var highlight = $('.wrap-left li:not(:first-child) a');
+    var dropdownLinks = $('.with-dropdown > a');
+    var dropMenu = $('.with-dropdown > .dropdown-menu');
+    var highlightDropLinks = $('.wrap-right .with-dropdown > a');
+
 
     // Add/Remove highlight class on click
-    highlight.click(function(){
-        highlight.removeClass('highlight');
-        $(this).addClass('highlight');
+    highlightText(highlight);
+    highlightText(highlightDropLinks);
+
+
+
+
+    // Show/Hide dropdown menus on click
+    dropdownLinks.click(function(){
+        // Selected menu
+        var actualDownMenu = $(this).next('.dropdown-menu');
+
+        // Reset already opened menu(if it exists)
+        dropMenu.not(actualDownMenu).hide();
+
+        // Show/Hide on click
+        actualDownMenu.toggle();
     });
 }); //end of document ready
+
+// FUNCTIONS
+
+// Add/Remove highlight class on click
+function highlightText(list) {
+    list.click(function(){
+        list.removeClass('highlight');
+        $(this).addClass('highlight');
+    });
+}
