1db8187497f54e48b1d0d57d0ba31e93b0f19e83
EmiPro97 <emi.proie97@gmail.com>
2021-04-11 22:55:47 +0200

    small changes

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var dropdownLinks = $('.with-dropdown > a');
     var dropMenu = $('.with-dropdown > .dropdown-menu');
     var highlightDropLinks = $('.wrap-right .with-dropdown > a');
+    console.log(highlightDropLinks);
 
 
     // Add/Remove highlight class on click
@@ -11,27 +12,35 @@ $(document).ready(function(){
     highlightText(highlightDropLinks);
 
 
-
-
     // Show/Hide dropdown menus on click
     dropdownLinks.click(function(){
         // Selected menu
-        var actualDownMenu = $(this).next('.dropdown-menu');
+        var currentDownMenu = $(this).next('.dropdown-menu');
 
         // Reset already opened menu(if it exists)
-        dropMenu.not(actualDownMenu).hide();
+        dropMenu.not(currentDownMenu).hide();
 
         // Show/Hide on click
-        actualDownMenu.toggle();
+        currentDownMenu.toggle();
     });
-}); //end of document ready
+    
+//end of document ready
+}); 
 
 // FUNCTIONS
 
 // Add/Remove highlight class on click
 function highlightText(list) {
+    var previouslndex = null;
     list.click(function(){
-        list.removeClass('highlight');
-        $(this).addClass('highlight');
+        currentIndex = list.index(this);
+        if (currentIndex != previouslndex){
+            list.removeClass('highlight');
+            $(this).addClass('highlight');
+            previouslndex = currentIndex;
+        }else{
+            $(this).toggleClass('highlight');
+        }
+        
     });
 }
